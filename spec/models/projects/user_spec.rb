4b7f52925be791d46585b127664f0d736896f418
Ivan Novikov <ivan.novikov@saltpepper.ru>
2015-12-05 19:14:12 +0300

    Add Projects::User spec

---
+++ spec/models/projects/user_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe Projects::User do
+  it { should belong_to(:project) }
+  it { should validate_presence_of(:project) }
+  it { should belong_to(:user) }
+  it { should validate_presence_of(:user) }
+  it { should validate_uniqueness_of(:user_id).scoped_to(:project_id) }
+end
