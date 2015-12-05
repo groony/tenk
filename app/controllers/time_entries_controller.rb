c2b1ad8f0854ce8cd9cc35ecb9f8de0549512208
Ivan Novikov <ivan.novikov@saltpepper.ru>
2015-12-06 00:12:16 +0300

    Add edit action TimeEntriesController

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -1,4 +1,11 @@
 class TimeEntriesController < ApplicationController
+  before_action :find_time_entry!, only: :edit
   def edit
+  end
+
+  private
+
+  def find_time_entry!
+    @time_entry = TimeEntries::Find.run!(params)
   end
 end
