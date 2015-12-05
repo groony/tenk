a35d1bdbbd67542fe4dc8e1daa2326d5694d22f7
Ivan Novikov <ivan.novikov@saltpepper.ru>
2015-12-05 23:15:32 +0300

    Add Projects index page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController < ApplicationController
   def index
+    @projects = Projects::List.run!(params)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root 'projects#index'
+  resources :projects, only: :show
   mount Sidekiq::Web, at: '/sidekiq'
 end
